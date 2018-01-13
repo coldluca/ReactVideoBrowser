ea913bddc3658eece7d8b24871b1283245bad3bc
Luca Bianco <bianco.luca.86@gmail.com>
2018-01-13 18:27:32 +0100

    ADD simple search_bar component with an input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 class App extends Component {
 	render() {
-		return <div>Hi!</div>
+		return (
+			<div>
+			Hi! <SearchBar />
+			</div>
+		);
 	}
 }
 
